5c40afd371d9257c8fea5ecf2debf106ee6ab350
Michael <gt.gluck@gmail.com>
2023-08-28 10:13:28 +0300

    fix: Chat component

--- front/src/components/Chat/Chat.tsx
+++ front/src/components/Chat/Chat.tsx
@@ -2,6 +2,7 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { CardGiftcard, EmojiEmotions, Gif } from '@mui/icons-material';
 
 import ChatHead from './ChatHead';
+import Message from './Message';
 
 import style from './Chat.module.scss';
 
@@ -11,7 +12,7 @@ const Chat = () => {
       <ChatHead />
 
       <div className={style.messages}>
-
+        <Message />
       </div>
       <div className={style.input}>
         <AddCircleIcon fontSize='large' />
